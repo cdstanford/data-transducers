269ddcb349a341653c6d32e1a6ad98528ea3334b
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-06-06 20:47:49 -0400

    unit tests for Ext values

--- src/ext_value.rs
+++ src/ext_value.rs
@@ -8,7 +8,7 @@ Ext<T> can be thought variant of Option<T>.
 
 extern crate derive_more;
 use derive_more::{Display, From};
-use std::ops;
+use std::{ops,i32};
 
 #[derive(Debug, PartialEq, Display, From, Copy, Clone)]
 pub enum Ext<T> {
@@ -17,6 +17,7 @@ pub enum Ext<T> {
     Many,
 }
 
+// Union operation
 impl<T: Copy> ops::Add for Ext<T> {
     type Output = Self;
 
@@ -54,6 +55,72 @@ fn apply2<T1, T2, T3>(op: fn(T1, T2) -> T3,
             Ext::One(y) => Ext::One(op(x, y)),
             Ext::Many => Ext::Many,
         },
-        Ext::Many => Ext::Many,
+        Ext::Many => match v2 {
+            Ext::None => Ext::None,
+            Ext::One(y) => Ext::Many,
+            Ext::Many => Ext::Many,
+        }
+    }
+}
+
+// ========== TESTS ==========
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_union() {
+        let x0 : Ext<i32> = Ext::None;
+        let x1 = Ext::One(3);
+        let x2 : Ext<i32> = Ext::Many;
+        assert_eq!(x0 + x0, x0);
+        assert_eq!(x0 + x1, x1);
+        assert_eq!(x1 + x0, x1);
+        assert_eq!(x0 + x2, x2);
+        assert_eq!(x2 + x0, x2);
+        assert_eq!(x1 + x2, x2);
+        assert_eq!(x2 + x1, x2);
+        assert_eq!(x2 + x2, x2);
+    }
+
+    #[test]
+    fn test_eq() {
+        assert_eq!(Ext::One(3), Ext::One(1 + 2));
+        assert_ne!(Ext::One(-1), Ext::One(1));
+        assert_ne!(Ext::One(0), Ext::None);
+        assert_ne!(Ext::None, Ext::One(0));
+        let mut x1 : Ext<i32> = Ext::None;
+        let x2 : Ext<i32> = Ext::None;
+        let x3 : Ext<i32> = Ext::Many;
+        assert_ne!(x2, x3);
+        assert_eq!(x1, x2);
+        assert_eq!(x1, x1);
+        // let x4 : Ext<i64> = Ext::None;
+        // assert_ne!(x2, x4); // Note: Type error
+    }
+
+    #[test]
+    fn test_apply() {
+        let mut x0 : Ext<i32> = Ext::None;
+        let x1 = Ext::One(3);
+        let mut x2 = Ext::One(2);
+        let x3 : Ext<i32> = Ext::Many;
+        assert_eq!(apply1(i32::count_ones, x0), Ext::None);
+        assert_eq!(apply1(i32::count_ones, x1), Ext::One(2));
+        assert_eq!(apply2(ops::Add::add, x1, x2), Ext::One(5));
+        assert_eq!(apply2(ops::Add::add, x2, x2), Ext::One(4));
+        assert_eq!(apply2(ops::Mul::mul, x1, x3), Ext::Many);
+        assert_eq!(apply2(ops::Mul::mul, x3, x0), Ext::None);
+        let y0 : Ext<&str> = Ext::None;
+        let y1 : Ext<&str> = Ext::from("hello");
+        let y2 : Ext<String> = Ext::from(String::from("world"));
+        assert_eq!(apply1(str::len, y0), Ext::None);
+        assert_eq!(apply1(str::len, y1), Ext::One(5));
+        assert_eq!(apply1(String::from, Ext::from("world")), y2);
+        assert_eq!(
+            apply1(Ext::from, Ext::One(5)),
+            Ext::One(Ext::One(5))
+        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -26,25 +26,6 @@ struct State<T> {
 fn main() {
     println!("=== Data Transducers Library ===");
 
-    // let mut three : i32 = 3;
-    let ev0 : Ext<i32> = Ext::None;
-    let ev1 = Ext::One(3);
-    let ev2 : Ext<i32> = Ext::Many;
-
-    println!("{}, {}, {}", ev0, ev1, ev2);
-
-    // let mut ev3 : Ext<i32> = Ext::None;
-    // let ev4 : Ext<i64> = Ext::None;
-    // let ev5 : Ext<i32> = Ext::One(4);
-    // 
-    // 
-    // assert!(ev1 != ev2);
-    // assert!(ev0 == ev3);
-    // 
-    // println!("{}", ev1 + ev5);
-
-    // assert!(ev3 == ev4);
-
     // let mut s1 = State::new(3);
     // let mut s2 = State::new(2);
     // let mut s2 = State::new(None);
