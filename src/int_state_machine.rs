6961b05b92e848d968da38d8b7382784e061bee3
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-08-22 18:47:10 -0400

    use liftimes to fix compilation -- investigate how to get update code to work later

--- src/int_state_machine.rs
+++ src/int_state_machine.rs
@@ -16,34 +16,34 @@ use std::vec::Vec;
 
 type State = Ext<i32>;
 
-struct Trans0 {
-    target: &State,
+struct Trans0<'a> {
+    target: &'a mut State,
     eval: fn() -> i32,
 }
-struct Trans1 {
-    source1: &State,
-    target: &State,
+struct Trans1<'a> {
+    source1: &'a State,
+    target: &'a mut State,
     eval: fn(i32) -> i32,
 }
-struct Trans2 {
-    source1: &State,
-    source2: &State,
-    target: &State,
+struct Trans2<'a> {
+    source1: &'a State,
+    source2: &'a State,
+    target: &'a mut State,
     eval: fn(i32, i32) -> i32,
 }
-enum Trans {
-    T0(Trans0),
-    T1(Trans1),
-    T2(Trans2),
+enum Trans<'a> {
+    T0(Trans0<'a>),
+    T1(Trans1<'a>),
+    T2(Trans2<'a>),
 }
 // X: character type for input string to the transducer
-struct Transition<X> {
-    t: Trans,
+struct Transition<'a, X> {
+    t: Trans<'a>,
     is_enabled: fn(&X) -> bool,
 }
-fn sources<X>(t: &Transition<X>) -> Vec<&State> {
+fn sources<'a, X>(t: &Transition<'a, X>) -> Vec<&'a State> {
     let mut vec = Vec::new();
-    match t.t {
+    match &t.t {
         Trans::T0(_) => (),
         Trans::T1(t1) => vec.push(t1.source1),
         Trans::T2(t2) => {
@@ -53,61 +53,61 @@ fn sources<X>(t: &Transition<X>) -> Vec<&State> {
     };
     vec
 }
-// fn target<X>(t: Transition<X>) -> State {
-//     match t.t {
-//         Trans::T0(t0) => t0.target,
-//         Trans::T1(t1) => t1.target,
-//         Trans::T2(t2) => t2.target,
-//     }
-// }
-// fn eval<X>(t: Transition<X>) -> Ext<i32> {
-//     match t.t {
-//         Trans::T0(t0) => ext_value::apply0(t0.eval),
-//         Trans::T1(t1) => ext_value::apply1(t1.eval, t1.source1),
-//         Trans::T2(t2) => ext_value::apply2(t2.eval, t2.source1, t2.source2),
-//     }
-// }
+fn target<'a, X>(t: &'a mut Transition<'a, X>) -> &'a mut State {
+    match &mut t.t {
+        Trans::T0(t0) => t0.target,
+        Trans::T1(t1) => t1.target,
+        Trans::T2(t2) => t2.target,
+    }
+}
+fn eval<'a, X>(t: &Transition<'a, X>) -> Ext<i32> {
+    match &t.t {
+        Trans::T0(t0) => ext_value::apply0(t0.eval),
+        Trans::T1(t1) => ext_value::apply1(t1.eval, *t1.source1),
+        Trans::T2(t2) => ext_value::apply2(t2.eval, *t2.source1, *t2.source2),
+    }
+}
 
-struct StateMachine<X> {
+struct StateMachine<'a, X> {
     n_states: usize,
     n_transitions: usize,
     states: Vec<State>,
     prev_states: Vec<State>,
-    transitions: Vec<Transition<X>>,
+    transitions: Vec<Transition<'a, X>>,
 }
 
-// impl<X> StateMachine<X> {
-//     fn reset_cur(&mut self) -> () {
-//         for x in &mut self.states {
-//             *x = Ext::None;
-//         };
-//     }
-//     fn update_prev(&mut self) -> () {
-//         for i in 0..(self.n_states) {
-//             self.prev_states[i] = self.states[i];
-//         }
-//     }
-//     fn reset(&mut self) -> () {
-//         self.reset_cur();
-//         self.update_prev();
-//     }
-//     fn update(&mut self, event: &X) -> () {
-//         /*
-//             Completely wrong implementation for now:
-//             Just evaluate all transitions, ignoring dependencies between them.
-//             This works as long as all transitions refer to only previous states
-//             as sources, never current states. So it doesn't allow e.g.
-//             epsilon transitions.
-//         */
-//         self.update_prev();
-//         self.reset_cur();
-//         // Not working, TODO fix
-//         // for t in &mut self.transitions {
-//         //     if (t.is_enabled)(event) {
-//         //         let s: State = target(t);
-//         //         s = s + eval(t);
-//         //         // target(t) = target(t) + eval(t);
-//         //     }
-//         // }
-//     }
-// }
+impl<'a, X> StateMachine<'a, X> {
+    fn reset_cur(&mut self) -> () {
+        for x in &mut self.states {
+            *x = Ext::None;
+        };
+    }
+    fn set_prev(&mut self) -> () {
+        for i in 0..(self.n_states) {
+            self.prev_states[i] = self.states[i];
+        }
+    }
+    fn reset(&mut self) -> () {
+        self.reset_cur();
+        self.set_prev();
+    }
+    fn update(&'a mut self, event: &X) -> () {
+        /*
+            Completely wrong implementation for now:
+            Just evaluate all transitions, ignoring dependencies between them.
+            This works as long as all transitions refer to only previous states
+            as sources, never current states. So it doesn't allow e.g.
+            epsilon transitions.
+        */
+        self.set_prev();
+        self.reset_cur();
+        // // Not working, TODO fix
+        // for t in &self.transitions {
+        //     if (t.is_enabled)(event) {
+        //         let s: &'a mut State = target(t);
+        //         *s = *s + eval(&t);
+        //         // target(t) = target(t) + eval(t);
+        //     }
+        // }
+    }
+}
