861d20679809cdf3ebcaf0651bb4573c3cfa90b0
Caleb Stanford <caleb.pirsquared@gmail.com>
2020-12-09 16:41:12 -0500

    Ext<T> product operation and other updates

--- src/ext_value.rs
+++ src/ext_value.rs
@@ -16,8 +16,9 @@ pub enum Ext<T> {
     Many,
 }
 
-// Union operation
-impl<T: Copy> ops::Add for Ext<T> {
+/* Union operation */
+
+impl<T> ops::Add for Ext<T> {
     type Output = Self;
 
     fn add(self, other: Self) -> Self {
@@ -33,11 +34,41 @@ impl<T: Copy> ops::Add for Ext<T> {
     }
 }
 
-pub fn apply0<T1>(op: fn() -> T1) -> Ext<T1> {
+/* Product (pair) operation */
+
+impl<T1, T2> ops::Mul<Ext<T2>> for Ext<T1> {
+    type Output = Ext<(T1, T2)>;
+
+    fn mul(self, rhs: Ext<T2>) -> Ext<(T1, T2)> {
+        match self {
+            Ext::None => Ext::None,
+            Ext::One(x) => match rhs {
+                Ext::None => Ext::None,
+                Ext::One(y) => Ext::One((x, y)),
+                Ext::Many => Ext::Many,
+            },
+            Ext::Many => match rhs {
+                Ext::None => Ext::None,
+                Ext::One(_) => Ext::Many,
+                Ext::Many => Ext::Many,
+            },
+        }
+    }
+}
+
+/* Function application */
+
+pub fn apply0<T1, F>(op: F) -> Ext<T1>
+where
+    F: Fn() -> T1,
+{
     Ext::One(op())
 }
 
-pub fn apply1<T1, T2>(op: fn(T1) -> T2, v1: Ext<T1>) -> Ext<T2> {
+pub fn apply1<T1, T2, F>(op: F, v1: Ext<T1>) -> Ext<T2>
+where
+    F: Fn(T1) -> T2,
+{
     match v1 {
         Ext::None => Ext::None,
         Ext::One(x) => Ext::One(op(x)),
@@ -45,27 +76,39 @@ pub fn apply1<T1, T2>(op: fn(T1) -> T2, v1: Ext<T1>) -> Ext<T2> {
     }
 }
 
-pub fn apply2<T1, T2, T3>(
-    op: fn(T1, T2) -> T3,
-    v1: Ext<T1>,
-    v2: Ext<T2>,
-) -> Ext<T3> {
-    match v1 {
-        Ext::None => Ext::None,
-        Ext::One(x) => match v2 {
-            Ext::None => Ext::None,
-            Ext::One(y) => Ext::One(op(x, y)),
-            Ext::Many => Ext::Many,
-        },
-        Ext::Many => match v2 {
-            Ext::None => Ext::None,
-            Ext::One(_) => Ext::Many,
-            Ext::Many => Ext::Many,
-        },
-    }
+pub fn apply2<T1, T2, T3, F>(op: F, v1: Ext<T1>, v2: Ext<T2>) -> Ext<T3>
+where
+    F: Fn(T1, T2) -> T3,
+{
+    apply1(|(x, y)| op(x, y), v1 * v2)
 }
 
-// ========== TESTS ==========
+pub fn apply3<T1, T2, T3, T4, F>(
+    op: F,
+    v1: Ext<T1>,
+    v2: Ext<T2>,
+    v3: Ext<T3>,
+) -> Ext<T4>
+where
+    F: Fn(T1, T2, T3) -> T4,
+{
+    apply1(|((x, y), z)| op(x, y, z), v1 * v2 * v3)
+}
+
+pub fn apply4<T1, T2, T3, T4, T5, F>(
+    op: F,
+    v1: Ext<T1>,
+    v2: Ext<T2>,
+    v3: Ext<T3>,
+    v4: Ext<T4>,
+) -> Ext<T5>
+where
+    F: Fn(T1, T2, T3, T4) -> T5,
+{
+    apply1(|(((x, y), z), t)| op(x, y, z, t), v1 * v2 * v3 * v4)
+}
+
+/* ========== TESTS ========== */
 
 #[cfg(test)]
 use std::i32;
@@ -90,6 +133,44 @@ mod tests {
     }
 
     #[test]
+    fn test_prod() {
+        let x0: Ext<i32> = Ext::None;
+        let x1 = Ext::One(3);
+        let x2 = Ext::One(5);
+        let x3: Ext<i32> = Ext::Many;
+        assert_eq!(x0 * x0, Ext::None);
+        assert_eq!(x0 * x1, Ext::None);
+        assert_eq!(x0 * x3, Ext::None);
+        assert_eq!(x2 * x0, Ext::None);
+        assert_eq!(x3 * x0, Ext::None);
+        assert_eq!(x1 * x1, Ext::One((3, 3)));
+        assert_eq!(x1 * x2, Ext::One((3, 5)));
+        assert_eq!(x2 * x1, Ext::One((5, 3)));
+        assert_eq!(x2 * x2, Ext::One((5, 5)));
+        assert_eq!(x1 * x3, Ext::Many);
+        assert_eq!(x3 * x2, Ext::Many);
+        assert_eq!(x3 * x3, Ext::Many);
+    }
+
+    #[test]
+    fn test_union_string() {
+        let x0: Ext<String> = Ext::None;
+        let x1: Ext<String> = Ext::One("Hello".to_owned());
+        let x2: Ext<String> = Ext::One("World".to_owned());
+        let x3: Ext<String> = Ext::Many;
+        assert_eq!(x0.clone() + x0.clone(), x0.clone());
+        assert_eq!(x0.clone() + x1.clone(), x1.clone());
+        assert_eq!(x1.clone() + x0.clone(), x1.clone());
+        assert_eq!(x1.clone() + x2.clone(), x3.clone());
+        assert_eq!(x0.clone() + x2.clone(), x2.clone());
+        assert_eq!(x0.clone() + x3.clone(), x3.clone());
+        assert_eq!(x3.clone() + x0.clone(), x3.clone());
+        assert_eq!(x1.clone() + x3.clone(), x3.clone());
+        assert_eq!(x3.clone() + x2.clone(), x3.clone());
+        assert_eq!(x3.clone() + x3.clone(), x3.clone());
+    }
+
+    #[test]
     fn test_eq() {
         assert_eq!(Ext::One(3), Ext::One(1 + 2));
         assert_ne!(Ext::One(-1), Ext::One(1));
@@ -101,8 +182,6 @@ mod tests {
         assert_ne!(x2, x3);
         assert_eq!(x1, x2);
         assert_eq!(x1, x1);
-        // let x4 : Ext<i64> = Ext::None;
-        // assert_ne!(x2, x4); // Note: Type error
     }
 
     #[test]
