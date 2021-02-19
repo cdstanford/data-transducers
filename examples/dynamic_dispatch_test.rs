6188cafc22ce74233cee9e601f6144e656ac4531
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-02-18 21:34:47 -0500

    expand dynamic dispatch example

--- examples/dynamic_dispatch_test.rs
+++ examples/dynamic_dispatch_test.rs
@@ -7,8 +7,8 @@ use std::fmt::Debug;
 trait DynamicType: Debug {
     fn simple_func(&self) -> usize;
     fn noop(&self);
-    // Note: dynamic typing is quite restrictive. None
-    // of the following work.
+    fn update(&mut self);
+    // Note: dynamic typing can be restrictive. The following don't work:
     // fn get_val(&self) -> &Self {
     //     self
     // }
@@ -22,13 +22,29 @@ impl DynamicType for String {
         0
     }
     fn noop(&self) {}
+    fn update(&mut self) {
+        self.push('a');
+    }
 }
 
 impl DynamicType for usize {
     fn simple_func(&self) -> usize {
-        1
+        *self
     }
     fn noop(&self) {}
+    fn update(&mut self) {
+        *self += 1;
+    }
+}
+
+impl<T: Debug + Default> DynamicType for Vec<T> {
+    fn simple_func(&self) -> usize {
+        self.len()
+    }
+    fn noop(&self) {}
+    fn update(&mut self) {
+        self.push(Default::default());
+    }
 }
 
 fn main() {
@@ -37,21 +53,27 @@ fn main() {
     v1.push(Box::new("Hello 1".to_owned()));
     v1.push(Box::new(100));
     v1.push(Box::new("World 1".to_owned()));
+    v1.push(Box::new(vec![100]));
     v2.push(Box::new("Hello 2".to_owned()));
     v2.push(Box::new(200));
     v2.push(Box::new("World 2".to_owned()));
+    v2.push(Box::new(vec![200, 200]));
     // Printing
     println!("v1: {:?}", v1);
     println!("v2: {:?}", v2);
     // Calling functions
     let mut simple_results1 = Vec::new();
-    for x1 in v1 {
+    for mut x1 in v1 {
         x1.noop();
+        simple_results1.push(x1.simple_func());
+        x1.update();
         simple_results1.push(x1.simple_func());
     }
     let mut simple_results2 = Vec::new();
-    for x2 in v2 {
+    for mut x2 in v2 {
         x2.noop();
+        simple_results2.push(x2.simple_func());
+        x2.update();
         simple_results2.push(x2.simple_func());
     }
     println!("simple results 1: {:?}", simple_results1);
