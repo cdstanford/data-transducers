fdeae3046f45f67ff00d902cd8beeef7528e5c06
Caleb Stanford <caleb.pirsquared@gmail.com>
2021-02-21 18:13:55 -0500

    interface fixes

--- src/interface.rs
+++ src/interface.rs
@@ -21,80 +21,90 @@ pub enum RInput<Init, Input> {
     Item(Input),
 }
 
-trait Transducer {
-    /* TYPES */
-
-    // Initial input
-    type Init;
-
-    // Input and output data streams
-    type Input;
-    type Output;
+trait Transducer<Init, Input, Output> {
+    /*
+        TYPES:
+        Init: initial input
+        Input: input data stream
+        Output: output produced for each input
+    */
 
     /* FUNCTIONALITY TO IMPLEMENT */
 
     // Computation
-    fn new() -> Self;
-    fn init(&mut self, i: Self::Init) -> Ext<Self::Output>;
-    fn update(&mut self, item: Self::Input) -> Ext<Self::Output>;
+    fn init(&mut self, i: Init) -> Ext<Output>;
+    fn update(&mut self, item: Input) -> Ext<Output>;
+    // Reset all computation back to the original state
+    fn reset(&mut self);
+    // Spawn an empty copy of the transducer: one that is in the initial
+    // state and prior to any .init() updates
+    // If Self: Clone, this should be roughly equivalent to self.clone().reset()
+    fn spawn_empty(&self) -> Self;
 
-    // Static information (type-associated data)
-    fn is_restartable() -> bool;
-    fn n_states() -> usize;
-    fn n_transs() -> usize;
+    // Static information
+    // This could be done with associated functions (type-associated data),
+    // but methods are more flexible as it will allow transducer implementations
+    // which do not encode the # of states and transitions as part of the type
+    fn is_restartable(&self) -> bool;
+    fn n_states(&self) -> usize;
+    fn n_transs(&self) -> usize;
 
     /* DERIVED FUNCTIONALITY */
 
     // Process an input stream (plus an initial value)
-    fn process_stream<I>(
-        i: Self::Init,
+    fn process_stream<'a, I>(
+        &'a mut self,
+        i: Init,
         mut strm: I,
-    ) -> Box<dyn Iterator<Item = Ext<Self::Output>>>
+    ) -> Box<dyn Iterator<Item = Ext<Output>> + 'a>
     // Sad output type because 'impl Iterator' is not allowed here :(
     where
-        I: 'static + Iterator<Item = Self::Input>,
-        Self: 'static + Sized,
+        I: Iterator<Item = Input> + 'a,
+        Self: Sized,
+        Output: 'a,
     {
-        let mut transducer = Self::new();
-        let y0 = transducer.init(i);
+        let y0 = self.init(i);
         Box::new(iter::once(y0).chain(iter::from_fn(move || {
-            strm.next().map(|item| transducer.update(item))
+            strm.next().map(|item| self.update(item))
         })))
     }
 
     // Process an input stream with "restart" events (initial values),
     // processing such events using one transducer and .init()
-    fn process_rstream_single<I>(
+    fn process_rstream_single<'a, I>(
+        &'a mut self,
         mut strm: I,
-    ) -> Box<dyn Iterator<Item = Ext<Self::Output>>>
+    ) -> Box<dyn Iterator<Item = Ext<Output>> + 'a>
     where
-        I: 'static + Iterator<Item = RInput<Self::Init, Self::Input>>,
-        Self: 'static + Sized,
+        I: Iterator<Item = RInput<Init, Input>> + 'a,
+        Self: Sized + 'a,
     {
-        let mut transducer = Self::new();
         Box::new(iter::from_fn(move || {
             strm.next().map(|item| match item {
-                RInput::Restart(i) => transducer.init(i),
-                RInput::Item(item) => transducer.update(item),
+                RInput::Restart(i) => self.init(i),
+                RInput::Item(item) => self.update(item),
             })
         }))
     }
 
     // Process an input stream with "restart" events, processing such
     // events by spawning many transducers
-    fn process_rstream_multi<I>(
+    // Doesn't use &self for any computation; instead
+    // uses .spawn_empty() to get an initial state for each new transducer
+    fn process_rstream_multi<'a, I>(
+        &'a self,
         mut strm: I,
-    ) -> Box<dyn Iterator<Item = Ext<Self::Output>>>
+    ) -> Box<dyn Iterator<Item = Ext<Output>> + 'a>
     where
-        I: 'static + Iterator<Item = RInput<Self::Init, Self::Input>>,
-        Self: 'static + Sized,
-        Self::Input: Clone,
+        I: Iterator<Item = RInput<Init, Input>> + 'a,
+        Self: Sized,
+        Input: Clone,
     {
         let mut transducers: Vec<Self> = Vec::new();
         Box::new(iter::from_fn(move || {
             strm.next().map(|item| match item {
                 RInput::Restart(i) => {
-                    transducers.push(Self::new());
+                    transducers.push(self.spawn_empty());
                     transducers.last_mut().unwrap().init(i)
                 }
                 RInput::Item(item) => {
@@ -110,17 +120,18 @@ trait Transducer {
 
     // Having defined the above, now we can write a function which tests whether
     // the restartability property correctly holds
-    fn check_restartability_for<I>(strm: I)
+    fn check_restartability_for<'a, I>(&'a self, strm: I)
     where
-        I: 'static + Iterator<Item = RInput<Self::Init, Self::Input>> + Clone,
-        Self: 'static + Sized,
-        Self::Input: Clone,
-        Self::Output: Eq,
+        I: Iterator<Item = RInput<Init, Input>> + Clone + 'a,
+        Self: Sized,
+        Input: Clone,
+        Output: Eq,
     {
-        if Self::is_restartable() {
-            let strm2 = strm.clone();
-            let single_out = Self::process_rstream_single(strm);
-            let multi_out = Self::process_rstream_multi(strm2);
+        if self.is_restartable() {
+            let mut self1 = self.spawn_empty();
+            let strm1 = strm.clone();
+            let single_out = self1.process_rstream_single(strm1);
+            let multi_out = self.process_rstream_multi(strm);
             assert!(single_out.eq(multi_out));
         } else {
             eprintln!(
